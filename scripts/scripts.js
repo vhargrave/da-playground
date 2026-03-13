71af59aa83742bbee2df3e5a46f90fe67d126633
Victor Hargrave <115231412+vhargrave@users.noreply.github.com>
2026-03-13 14:11:03 +0100

    Initial commit

---
+++ scripts/scripts.js
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2022 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+export const [setLibs, getLibs] = (() => {
+  let libs;
+  return [
+    (prodLibs, location) => {
+      libs = (() => {
+        const { hostname, search } = location || window.location;
+        if (!(hostname.includes('.aem.') || hostname.includes('local'))) return prodLibs;
+        const branch = new URLSearchParams(search).get('milolibs') || 'main';
+        if (branch === 'local') return 'http://localhost:6456/libs';
+        return branch.includes('--') ? `https://${branch}.aem.live/libs` : `https://${branch}--milo--adobecom.aem.live/libs`;
+      })();
+      return libs;
+    }, () => libs,
+  ];
+})();
+
+
+function decorateArea(area = document) {
+  const eagerLoad = (parent, selector) => {
+    const img = parent.querySelector(selector);
+    img?.removeAttribute('loading');
+  };
+
+  (async function loadLCPImage() {
+    const marquee = document.querySelector('.marquee');
+    if (!marquee) {
+      eagerLoad(document, 'img');
+      return;
+    }
+  
+    // First image of first row
+    eagerLoad(marquee, 'div:first-child img');
+    // Last image of last column of last row
+    eagerLoad(marquee, 'div:last-child > div:last-child img');
+  }());
+}
+
+// Add project-wide style path here.
+const STYLES = '';
+
+// Use 'https://www.adobe.com/libs' if you cannot map '/libs' to milo's origin and ensure you don't run into CORS issues.
+const LIBS = '/libs';
+
+// Add any config options.
+const CONFIG = {
+  // codeRoot: '',
+  // contentRoot: '',
+  // imsClientId: 'college',
+  // imsScope: 'AdobeID,openid,gnav',
+  // geoRouting: 'off',
+  // fallbackRouting: 'off',
+  // iconsExcludeBlocks: [],
+  decorateArea,
+  locales: {
+    '': { ietf: 'en-US', tk: 'hah7vzn.css' },
+    de: { ietf: 'de-DE', tk: 'hah7vzn.css' },
+    kr: { ietf: 'ko-KR', tk: 'zfo3ouc' },
+  },
+};
+
+// Decorate the page with site specific needs.
+decorateArea();
+
+/*
+ * ------------------------------------------------------------
+ * Edit below at your own risk
+ * ------------------------------------------------------------
+ */
+
+const miloLibs = setLibs(LIBS);
+
+(function loadStyles() {
+  const paths = [`${miloLibs}/styles/styles.css`];
+  if (STYLES) { paths.push(STYLES); }
+  paths.forEach((path) => {
+    const link = document.createElement('link');
+    link.setAttribute('rel', 'stylesheet');
+    link.setAttribute('href', path);
+    document.head.appendChild(link);
+  });
+}());
+
+(async function loadPage() {
+  const { loadArea, setConfig } = await import(`${miloLibs}/utils/utils.js`);
+  const config = setConfig({ ...CONFIG, miloLibs });
+  console.log(config);
+  await loadArea();
+}());
